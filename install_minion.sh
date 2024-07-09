3f51b2f8a8392a710be2e04a06d8e8b0ff5851da
greenwoo <anton.mazur@jatapp.com>
2024-07-09 15:16:10 +0300

    Modify script

--- install_minion.sh
+++ install_minion.sh
@@ -7,3 +7,20 @@ sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.
 echo deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/20.04/amd64/3006 focal main | sudo tee /etc/apt/sources.list.d/salt.list
 
 sudo apt update && sudo apt -y install salt-minion
+
+MASTER_CONF_CONTENT="master:
+  - salty.veepn.com"
+
+DEST_FILE="/etc/salt/minion.d/master.conf"
+
+mkdir -p $(dirname "$DEST_FILE")
+
+echo "$MASTER_CONF_CONTENT" > "$DEST_FILE"
+
+read -p "Enter hostname for minion_id: " HOSTNAME
+
+MINION_ID_FILE="/etc/salt/minion_id"
+
+echo "$HOSTNAME" > "$MINION_ID_FILE"
+
+systemctl restart salt-minion.service
